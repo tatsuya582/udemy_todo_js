3e3add36638173e48855dc3e46ea42509b8a5056
tatsuya582 <y.k.tsutaya.com_0313@icloud.com>
2024-08-15 17:17:29 +0900

    add: タスクの削除機能を追加

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ const onClickAdd = () => {
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "削除";
     deleteButton.addEventListener("click", () => {
-        alert("削除");
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
     })
 
     div.appendChild(p);
