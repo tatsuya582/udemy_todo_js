68c9c8a61995a74f420241b9a437d649c7600a99
tatsuya582 <y.k.tsutaya.com_0313@icloud.com>
2024-08-15 16:53:32 +0900

    add: タスクの追加機能を仮実装

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+const onClickAdd = () => {
+    // テキストボックスの値の取得と初期化
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+    
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    div.appendChild(p);
+    li.appendChild(div);
+
+    console.log(li)
+
+    document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
